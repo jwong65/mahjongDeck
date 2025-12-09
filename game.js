990bbece0bd6a0756ea8d2d44487f166e00edbd7
jwong65 <jason.wong6507@gmail.com>
2025-12-09 17:45:17 -0500

    Added frontTile and backTile to the constructor planning for images

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ class Tiles{
     constructor(suit, value){
         this.suit = suit;
         this.value = value; 
+        this.frontTile = frontTilePath
+        this.backTile = backTilePath
     }
     toString(){
         return `${this.suit} + ${this.value}`;
